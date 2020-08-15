29e51ab98fa3ba1cd7020d8681c05d3bc97cc590
lalebdi <loai.alebdi@gmail.com>
2020-08-15 15:22:55 -0400

    implemented Material Ui and tested the responsiveness

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import NewsCard from '../NewsCard/NewsCard';
+import { Grid, Grow, Typography } from '@material-ui/core';
 
 const NewsCards = ({articles}) => {
     return (
-        <div>
+        <Grow in>
+            <Grid container alignItems="stretch" spacing={3} >
+
             {articles.map((article, i) =>(
-                <NewsCard />
-            ))}
-        </div>
+                <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex'}}>
+                <NewsCard article={article} i={i} />
+                </Grid>
+                ))}
+                
+            </Grid>
+        </Grow>
     )
 }
 
