43c00fcdf1e8552f21edc53292aba074a072c8c3
lalebdi <loai.alebdi@gmail.com>
2020-08-15 14:29:57 -0400

    first test of the voice command works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+require('dotenv').config()
+
+const alanKey = process.env.REACT_APP_ALAN;
+
 
 export default function App() {
+// useEffect takes 2 params. one is a callback and the second is a dependency array. if the array is empty it will run once when the it mounts.
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command}) => {
+                if(command === 'testCommand'){
+                    alert('executed')
+                }
+            }
+        })
+    }, [])
+
     return (
         <div>
             <h1> Alan AI News App </h1>
