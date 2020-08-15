f247fd9b2462c28b135c27fde3955918c6d280e4
lalebdi <loai.alebdi@gmail.com>
2020-08-15 15:12:42 -0400

    added cards and worked during the testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
 require('dotenv').config()
 
 const alanKey = process.env.REACT_APP_ALAN;
 
 
 export default function App() {
+    const [newsArticles, setNewsArticles] = useState([]);
 // useEffect takes 2 params. one is a callback and the second is a dependency array. if the array is empty it will run once when the it mounts.
     useEffect(() => {
         alanBtn({
             key: alanKey,
             onCommand: ({command, articles}) => {
                 if(command === 'newHeadlines'){
-                    console.log(articles)
+                    // console.log(articles)
+                    setNewsArticles(articles);
                 }
             }
         })
@@ -21,6 +24,7 @@ export default function App() {
     return (
         <div>
             <h1> Alan AI News App </h1>
+            <NewsCards articles={newsArticles} />
         </div>
     )
 }
