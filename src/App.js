798809aa3ca4f1a46895a6570f7b1381a0b54379
lalebdi <loai.alebdi@gmail.com>
2020-08-15 17:11:01 -0400

    styled the home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles'
 require('dotenv').config()
 
 const alanKey = process.env.REACT_APP_ALAN;
@@ -8,6 +9,7 @@ const alanKey = process.env.REACT_APP_ALAN;
 
 export default function App() {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 // useEffect takes 2 params. one is a callback and the second is a dependency array. if the array is empty it will run once when the it mounts.
     useEffect(() => {
         alanBtn({
@@ -23,7 +25,9 @@ export default function App() {
 
     return (
         <div>
-            <h1> Alan AI News App </h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     )
